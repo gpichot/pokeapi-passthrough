797ed54e9bc087737b52386017e16bb4d17c557c
Gabriel Pichot <gabriel.pichot@gmail.com>
2022-10-04 18:48:07 +0200

    fix: hostname

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
+const hostname = process.env.HOSTNAME || "0.0.0.0";
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, hostname, () => {
+  console.log(`Listening on http://${hostname}:${port}`);
+});
